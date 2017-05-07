a1bfbde57a8b539f7c0d1a969cd901ef9616b562
booleanbob <trurltheinventor@gmail.com>
2017-05-06 22:01:24 -0400

    started building functions file

---
+++ joshfunc.py
@@ -0,0 +1,19 @@
+#Josh functions
+
+def help_book():
+	print("Here are some basic instructions for the game:\n")
+	print("Each room will list basic directions you can travel in. " + 
+	"Just type 'Go north' or 'Go door' to go that way\n" +
+	"You can check your inventory anytime by entering 'Pockets' at the prompt. \n" +
+	"Type Stats to check your stats \n" +
+	"For simplicity, there are only a few commands to remember: \n" +
+	" 'LOOK object' to examine things \n 'YOINK item' to pick up an item \n 'OPEN object' to open something \n" +
+	" 'USE item with object' to use an item you have with a target \n " +
+	"And finally, 'FIGHT person/monster/destiny' when the time comes \n"
+	"To consult this manual in the future, type 'Help Book' \n")
+	
+def battle_sys(enemy):
+	print("You draw your weapon and prepare to square off against a fearsome " + enemy + "!")
+	
+
+
